d279fd6f1888c87c7b419ec24ede175e864fc8a8
ls <davidisan67@gmail.com>
2025-09-03 15:09:00 +0800

    update

--- metric/Collision Rate/run.sh
+++ metric/Collision Rate/run.sh
@@ -3,11 +3,8 @@
 
 # 2) 运行
 python metric\Collision Rate\stp3_eval_nuscenes.py \
-  # 本地nuscenes数据集路径
   --nusc_root ...\nuscenes \ 
-  # 评测版本
   --version v1.0-mini \
-  # 预测文件路径
   --preds metric\Collision Rate\preds.json \
   --use_gt_yaw_each_step \
   --filter_gt_colliding
